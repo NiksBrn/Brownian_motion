5cb79359afd45b3bccc19e338f3f448819e867f1
NiksBrn <niks_kam@vk.com>
2025-03-18 22:35:06 +0300

    update src

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <GL/freeglut.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+#include "RigidBody.h"
+#include "Render.h"
+#include "Motion.h"
+
+int main(int argc, char** argv)
+{
+    Run(argc, argv);
+    return 0;
+}
